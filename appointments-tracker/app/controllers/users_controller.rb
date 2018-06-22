096de31f32c7053edb190269bbc560785ae0de01
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-22 22:27:17 +0000

    add get sign up route

--- appointments-tracker/app/controllers/users_controller.rb
+++ appointments-tracker/app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect "/appointments"
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+  post '/signup' do
+
+  end
 end
