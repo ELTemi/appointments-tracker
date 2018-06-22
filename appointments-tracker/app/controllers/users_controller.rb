ea07564c4aff5cf3f05a690296527eefe0583afc
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-22 23:09:06 +0000

    add post signup route and flash message

--- appointments-tracker/app/controllers/users_controller.rb
+++ appointments-tracker/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
+require "rack-flash"
+
 class UsersController < ApplicationController
+
+  use Rack::Flash
 
   get '/signup' do
     if logged_in?
@@ -9,6 +13,17 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
+    @user = User.new(params)
+    if @user.save
+      session[:id] = @user.id
+      redirect :'/appointments'
+    else
+      flash[:message] = "Either username/name/email already exists. Please try again!"
+      redirect :'/signup'
+    end
+  end
+
+  get '/login' do
 
   end
 end
--- appointments-tracker/app/models/user.rb
+++ appointments-tracker/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :username, :email
+  validates_uniqueness_of :name, :username, :email
   has_secure_password
   has_many :users
 
