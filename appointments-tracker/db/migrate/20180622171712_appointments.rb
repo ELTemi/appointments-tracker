a2e005c1a4e11ced0ce5a3c01bea15ded38c6509
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-22 17:55:00 +0000

    create tables for users and appointments

--- appointments-tracker/db/migrate/20180622171712_appointments.rb
+++ appointments-tracker/db/migrate/20180622171712_appointments.rb
@@ -1,4 +1,12 @@
 class Appointments < ActiveRecord::Migration
   def change
+    create_table :appointments do |t|
+      t.string :title
+      t.datetime :date
+      t.text :location
+      t.text :details
+      t.boolean :status
+      t.timestamps null: false
+     end
   end
 end
