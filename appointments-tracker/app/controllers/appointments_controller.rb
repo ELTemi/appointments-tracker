1f1d751570a588c07b7efe810c787b24faf774d4
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-22 23:47:49 +0000

    add appointments route

--- appointments-tracker/app/controllers/appointments_controller.rb
+++ appointments-tracker/app/controllers/appointments_controller.rb
@@ -1,2 +1,13 @@
 class AppointmentsController < ApplicationController
+
+  get '/appointments' do
+    if logged_in?
+      @user = current_user
+      @appointments = Appointment.all
+      erb: '/appointments/appointments'
+    else
+      flash[:message] = "Please login to view appointments"
+      redirect '/login'
+    end
+  end
 end
