2f525917b97ffd11f2a474b9f07228561fdf3186
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-22 17:38:13 +0000

    set up associations and validation gems

--- appointments-tracker/app/models/user.rb
+++ appointments-tracker/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  validates_presence_of :name, :username, :email
+  has_secure_password
+  has_many :users
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
 end
